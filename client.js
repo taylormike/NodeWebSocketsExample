a637c4fbdaa07f8523c9da478bd1566f9f18d244
taylormike <mike.j.taylor0@gmail.com>
2016-06-19 23:41:18 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,14 +1,13 @@
 var WebSocket = require('ws')
   , ws = new WebSocket('ws://localhost:8080/');
 
-var iters = 100000;
+var iters = 100;
 ws.on('open', function() {
     for (var  i = 0; i < iters; i++)
     {
         ws.send('something');
-        ws.send('something');
     }
 });
 ws.on('message', function(message) {
-    //console.log('received: %s', message);
+    console.log('received: %s', message);
 }); 
